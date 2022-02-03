760cfa3bf9771e6999a5050350be1567e5a232dc
Anthony Iroegbu <anthony@crenettechlabs.com>
2022-02-04 00:53:51 +0100

    setup server to utilize slack event and web apis

---
+++ server.js
@@ -0,0 +1,26 @@
+require('dotenv').config({ path: __dirname + '/.env' })
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+
+//YOUR CODE HERE
+
+const token = process.env.BOT_TOKEN
+const eventsApi = require('@slack/events-api')
+const slackEvents = eventsApi.createEventAdapter(process.env.SIGNING_SECRET)
+
+const { WebClient, LogLevel } = require('@slack/web-api');
+const client = new WebClient(token, {
+    logLevel: LogLevel.DEBUG
+});
+app.use('/', slackEvents.expressMiddleware())
+slackEvents.on('message', async (event) => {
+    console.log(event)
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`Slack Bot App listening at http://localhost:${PORT}`)
+})
