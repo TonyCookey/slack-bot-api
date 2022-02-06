52c994ed8e9600c841dc72481a1d19afcb254506
Anthony Iroegbu <anthony@crenettechlabs.com>
2022-02-06 21:25:58 +0100

    seperated code into server.js and app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan');
+
+const app = express()
+
+
+const responsesController = require('./controllers/responses.controller')
+
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+}));
+app.use(morgan('combined'));
+
+app.use(express.json());
+
+app.get('/users/responses/:user_email', responsesController.GetUserResponse)
+app.get('/users/responses', responsesController.GetAllResponses)
+
+app.get('/users/responses/:question/:user_email', responsesController.GetUserQuestionResponse)
+app.post('/users/responses', responsesController.SaveUserResponses)
--- server.js
+++ server.js
@@ -1,34 +1,14 @@
-require('dotenv').config({ path: __dirname + '/.env' })
+require('dotenv').config()
+
 const http = require('http');
-
-
-const express = require('express')
-const cors = require('cors')
-const morgan = require('morgan');
-
-const app = express()
-
-const server = http.createServer(app);
-
-const responsesController = require('./controllers/responses.controller')
-
 
 const { mongoConnect } = require('./mongo');
 
+const app = require('./app')
+
 const PORT = process.env.PORT || 3000
 
-app.use(cors({
-    origin: 'http://localhost:3000',
-}));
-app.use(morgan('combined'));
-
-app.use(express.json());
-
-app.get('/users/responses/:user_email', responsesController.GetUserResponse)
-app.get('/users/responses', responsesController.GetAllResponses)
-
-app.get('/users/responses/:question/:user_email', responsesController.GetUserQuestionResponse)
-app.post('/users/responses', responsesController.SaveUserResponses)
+const server = http.createServer(app);
 
 
 async function startServer() {
