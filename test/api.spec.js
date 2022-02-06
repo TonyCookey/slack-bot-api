4a75f4f43e940d0669a951561d0a8bb8a1d20629
Anthony Iroegbu <anthony@crenettechlabs.com>
2022-02-07 00:16:33 +0100

    completed api tests for all API endpoints

--- test/api.spec.js
+++ test/api.spec.js
@@ -5,6 +5,12 @@ const {
     mongoDisconnect,
 } = require('../mongo');
 
+const userResponse = {
+    question: "What is your favoorite hobby",
+    user_email: "tonyiroegbu59",
+    response: "Basketball"
+}
+
 
 describe('Slack Bot API', () => {
     beforeAll(async () => {
@@ -16,10 +22,38 @@ describe('Slack Bot API', () => {
     });
 
 
-    describe('Test GET all responses', () => {
+    describe('Test GET All User Responses', () => {
         test('It should respond with 200 success', async () => {
             const response = await request(app)
                 .get('/users/responses')
+                .expect('Content-Type', /json/)
+                .expect(200);
+        });
+    });
+    describe('Test POST Save New User Response', () => {
+
+        test('It should respond with 201 created', async () => {
+            const response = await request(app)
+                .post('/users/responses')
+                .send(userResponse)
+                .expect('Content-Type', /json/)
+                .expect(201);
+        });
+    })
+
+    describe('Test GET User Responses', () => {
+        test('It should respond with 200 success', async () => {
+            const response = await request(app)
+                .get(`/users/responses/${userResponse.user_email}`)
+                .expect('Content-Type', /json/)
+                .expect(200);
+        });
+    });
+
+    describe('Test GET User Question Response', () => {
+        test('It should respond with 200 success', async () => {
+            const response = await request(app)
+                .get(`/users/responses/${userResponse.user_email}/${userResponse.question}`)
                 .expect('Content-Type', /json/)
                 .expect(200);
         });
